edfc12fb55b18faad756ba9cc3cc8eaddad13d63
Gauri veer <gauriveer55@gmail.com>
2026-04-25 14:42:58 +0530

    initial commit

---
+++ cgpa.java
@@ -0,0 +1,12 @@
+public class cgpa {
+    public static void main(String []args){
+        Float subject1=45f;
+     Float subject2=35f;
+        Float subject3=56f;
+        Float cgpa=(subject1+subject2+subject3)/30;
+
+        System.out.println(cgpa);
+
+    }
+    
+}
