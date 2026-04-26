186187998d5c55b0c0f32ce1b609b46baae95428
Gauri veer <gauriveer55@gmail.com>
2026-04-26 11:02:14 +0530

    initial commit

---
+++ Name.java
@@ -0,0 +1,8 @@
+public class Name{
+    public static void main(String[]args){
+        System.out.println("What is your Name");
+        Scanner Sc=new Scanner(System.in);
+        String name=sc.Next();
+        System.out.println("Hello"+Name+"HAve a good day!");
+    }
+}
