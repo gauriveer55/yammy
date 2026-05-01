4b107339be92933876e2469dd47870d1bb1ada69
Gauri veer <gauriveer55@gmail.com>
2026-05-01 11:38:13 +0530

    initial commit

---
+++ Integer.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Integer {
+    public static void main (String []args){
+        System.out.println("Enter your Nmubers");
+        Scanner Sc=new Scanner(System.in);
+        System.out.println (Sc.hasNextInt());
+
+    }
+    
+}
