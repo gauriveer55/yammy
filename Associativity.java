19f1c6e17284b66b82794709f2ff7f6d9bc33206
Gauri veer <gauriveer55@gmail.com>
2026-05-02 18:47:23 +0530

    initial commit

---
+++ Associativity.java
@@ -0,0 +1,18 @@
+public class Associativity {
+    public static void main(String []args){
+        int a=6*5-34/2;
+        /*Hight precedance goes to *&/. The evalute L-R
+        =30-34/2
+        =30-17
+        =13 */
+        int b=60/5-34*2;
+        /*
+        =12-34*2
+        =12-68
+        =-56 */
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+    
+}
