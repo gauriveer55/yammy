bb5d90b9f0bdf8880c22e4b6453e785f8830f942
Gauri veer <gauriveer55@gmail.com>
2026-05-02 18:36:29 +0530

    initial commit

---
+++ opearators.java
@@ -0,0 +1,13 @@
+public class opearators {
+    public static void main(String []args){
+        int a=4;
+        int b=9;
+        b*=3;
+        //System.out.println(b);
+        //System.out.println(6==8);
+        System.out.println(64>5 && 64>98);
+        System.out.println(64>5 || 64>98);
+
+    }
+    
+}
