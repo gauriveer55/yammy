9c1fe30f5124f2a09bb421a2ee82a631ec84be8a
Gauri veer <gauriveer55@gmail.com>
2026-05-03 18:31:02 +0530

    initial cammit

---
+++ Quiz.java
@@ -0,0 +1,17 @@
+
+
+public class Quiz {
+    public static void main(String []args){
+        //int x=6;
+        //int y=1;
+        //int k=x*y/2;
+        //System.out.println(k);
+
+        int b=1;
+        int c=4;
+        int a=5;
+        int k=b*b-(4*a*c)/(2*a);
+        System.out.println(k);
+    }
+    
+}
