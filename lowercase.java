7401d8bf835a8acf308d11ab39947cd3ca8599aa
Gauri veer <gauriveer55@gmail.com>
2026-05-06 14:44:43 +0530

    initial commit

---
+++ lowercase.java
@@ -0,0 +1,9 @@
+public class lowercase {
+    public static void main(String []args){
+        String name ="Gauri Veer";
+        name=name.toLowerCase();
+        System.out.println(name);
+
+    }
+    
+}
