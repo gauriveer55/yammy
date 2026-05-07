09b25f267b391cd26dac13f517e352ac270f58a0
Gauri veer <gauriveer55@gmail.com>
2026-05-07 12:07:30 +0530

    initial commit

--- letter.java
+++ letter.java
@@ -1,8 +1,11 @@
 public class letter {
     public static void main(String[] args) {
-        String letter="Dear <|name|>, Thanks a lot !";
-        letter=letter.replace("<|name|>", "Gauri");
-        System.out.println(letter);
+        // String letter="Dear <|name|>, Thanks a lot !";
+        //letter=letter.replace("<|name|>", "Gauri");
+        //System.out.println(letter);
+
+        String myletter="Dear Harry,\n\t this java course is nice\n\t Thanks!";
+        System.out.println(myletter);
     }
 }
     
