a6b8b8530a79693c5a2370296e7f7241d467e1a1
Gauri veer <gauriveer55@gmail.com>
2026-05-07 11:59:09 +0530

    initial Commit

---
+++ space.java
@@ -0,0 +1,8 @@
+public class space {
+public static void main(String []args){
+    String mystring="This string contains double and  triple spaces";
+    System.out.println(mystring.indexOf ("  "));
+    System.out.println(mystring.indexOf("   "));
+
+}    
+}
