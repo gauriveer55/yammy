4d394bba8c861f3fff34f5d396763c204d484797
Gauri veer <gauriveer55@gmail.com>
2026-05-09 14:37:10 +0530

    initial commit

--- week.java
+++ week.java
@@ -7,36 +7,16 @@ public class week {
         int day =Sc.nextInt();
         switch (day){
 
-            case 1:
-            System.out.print("Monday");
-            break;
-
-            case 2:
-            System.out.print("Tuesday");
-            break;
-
-            case 3:
-            System.out.print("Wensday");
-            break;
-
-            case 4:
-            System.out.print("Thursday");
-            break;
-
-            case 5:
-            System.out.print("Friday");
-            break;
-
-            case 6:
-            System.out.print("Saturday");
-            break;
-
-            case 7:
-            System.out.print("Sunday");
-            break;
-            default:
-
-            System.out.println("Invalid day");
+            case 1: System.out.print("Monday");
+            
+            case 2:System.out.print("Tuesday");
+            case 3:System.out.print("Wensday");
+            case 4:System.out.print("Thursday");
+            case 5:System.out.print("Friday");
+            case 6:System.out.print("Saturday");
+            case 7:System.out.print("Sunday");
+          
+            
         }
     }
     
