86a11507f2d8669daaad05de0719f1018f17eaef
Gauri veer <gauriveer55@gmail.com>
2026-05-10 11:37:20 +0530

    initial commit

--- star.java
+++ star.java
@@ -1,8 +1,8 @@
 public class star {
     public static void main(String[] args) {
         int n=4;
-        for ( int i=1; i<=n; i++){
-            for (int j=1; j<=i; j++){
+        for ( int i=n; i>0; i--){
+            for (int j=0; j<i; j++){
                 System.out.print("*");
             }
             System.out.println();
