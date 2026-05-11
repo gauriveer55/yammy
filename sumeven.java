b9baa8fcec7c8c8b10c2229d7618d74646143579
Gauri veer <gauriveer55@gmail.com>
2026-05-11 11:46:21 +0530

    initial commit

--- sumeven.java
+++ sumeven.java
@@ -1,11 +1,23 @@
 public class sumeven {
     public static void main (String[]args){
-        int sum=0;
+        //int sum=0;
+        //int n=3;
+        //for (int i = 0; i <n; i++) {
+          //  sum=sum+(2*i);
+        //}
+        //System.out.println("Sum of even number is"+sum);
+
+
+        //What is the  FActorial of the n=n*n-1*n-2.....1
+        //5!=5*4*3*2*1
         int n=3;
-        for (int i = 0; i <n; i++) {
-            sum=sum+(2*i);
+        int i=1;
+        int factorial=1;
+        while(i<=n){
+            factorial *=i;
+            i++;
         }
-        System.out.println("Sum of even number is"+sum);
+        System.out.println("Factorial="+factorial);
     }
     }
     
