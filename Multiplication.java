0c18956e0292336c2ba46796c2fa80c4ef90d552
Gauri veer <gauriveer55@gmail.com>
2026-05-11 11:24:40 +0530

    initial commit

--- Multiplication.java
+++ Multiplication.java
@@ -1,9 +1,14 @@
 public class Multiplication {
     public static void main (String[]args){
-        int n=5;
-        for (int i=1; i<=10; i++){
-            System.out.printf("%d*%d=%d\n",n,i,n*i);
+        //int n=5;
+        //for (int i=1; i<=10; i++){
+            //System.out.printf("%d*%d=%d\n",n,i,n*i);
+        int n=8;
+        int sum=0;
+        for(int i=1;i<=10;i++){
+            sum+=n*i;
+        }
+        System.out.println(sum);
         }
     }
     
-}
