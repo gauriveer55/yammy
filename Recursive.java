493050a65976f684f855b59bb96cff1c0516b72a
Gauri veer <gauriveer55@gmail.com>
2026-05-17 18:48:05 +0530

    initial commit

---
+++ Recursive.java
@@ -0,0 +1,13 @@
+public class Recursive {
+    static int sumRec(int n){
+        if (n==1){
+            return 1;
+        }
+        return n+ sumRec(n-1);
+        }
+        public static void main(String[] args) {
+            int c=sumRec(4);
+            System.out.println(c);
+        }
+    
+}
